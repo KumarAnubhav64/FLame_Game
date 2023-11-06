d6b7f95a20b4bd74178db4d4090f07aa0936a6bc
KumarAnubhav64 <145448152+KumarAnubhav64@users.noreply.github.com>
2023-11-06 11:59:56 +0530

    Add start screen On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Screen.py
    	new file:   images/Aff.png
    	modified:   images/Ene.png
    	modified:   images/Friend.png
    	new file:   images/Marraige.png
    	new file:   images/OIP.jpeg
    	new file:   images/Picture2.png
    	new file:   images/Reset_on_press.png
    	new file:   images/Reset_on_release.png
    	new file:   images/Splash.png
    	new file:   images/Start.png
    	new file:   images/Start_re.png
    	modified:   images/love.png
    	modified:   screen.kv
    	modified:   tempCodeRunnerFile.py

--- Screen.py
+++ Screen.py
@@ -1,5 +1,4 @@
 # Flames App made on kivy framework by Kumar Anubhav
-
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -12,8 +11,9 @@ from kivy.properties import StringProperty
 Window.size = (360, 700)
 # define our differnt screen
 
-class SplashWindow(Screen):
-        pass
+class LoadWindow(Screen):
+    def start_release(self):
+        self.ids.Start.source = 'images/Start_re.png'
 class FirstWindow(Screen):
     def FlameLogic(self):
         him_name = self.ids.Him.text
@@ -26,29 +26,51 @@ class FirstWindow(Screen):
         for i in her_name:
             if i not in him_name:
                 uncommon_char.append(i)
+        fla = ["Frindship","Love","Affection","Marraige","Enemies"]
         flame = len(uncommon_char)
+        ref_to_other_screen = self.manager.get_screen("ResultWindow")
+        
         pos_flame = flame % 6
         if pos_flame == 0:
-            self.ids.Match.source = 'images/Friend.png'
-        elif pos_flame == 1:
-            self.ids.Match.source = 'images/love.png'
+            ref_to_other_screen.ids.Result_Label.text= fla[0]
+            ref_to_other_screen.ids.result_image.source= "images/Friend.png"
+        elif pos_flame == 1:                                                                                                                                                        
+            ref_to_other_screen.ids.Result_Label.text = fla[1]
+            ref_to_other_screen.ids.result_image.source= "images/love.png"
         elif pos_flame == 2:
-            self.ids.Match.source = 'images/Affection.png'
+            ref_to_other_screen.ids.Result_Label.text = fla[2]
+            ref_to_other_screen.ids.result_image.source= "images/Aff.png"
         elif pos_flame == 3:
-            self.ids.Match.source = 'images/mar.png'
+            ref_to_other_screen.ids.Result_Label.text= fla[3]
+            ref_to_other_screen.ids.result_image.source= "images/Marraige.png"
         elif pos_flame == 4:
-            self.ids.Match.source = 'images/Ene.png'
-        # return self.pos_flame
+            ref_to_other_screen.ids.Result_Label.text = fla[4]
+            ref_to_other_screen.ids.result_image.source= "images/Ene.png"
+        # if pos_flame == 0:
+        #     self.manager.get_screen('ResultWindow').ids.
+        # elif pos_flame == 1:                                                                                                                                                        
+        #     self.ids.Match.text = fla[1]
+        # elif pos_flame == 2:
+        #     self.ids.Match.text = fla[2]
+        # elif pos_flame == 3:
+        #     self.ids.Match.text = fla[3]
+        # elif pos_flame == 4:
+        #     self.ids.Match.text = fla[4]
 
     def release(self):
         self.ids.Button_Image.source = 'images/Button_not_pressed.png'
 
-    # def Match(self, pos_flame):
-    #     wp = ResultWindow(size_hint=(None, None), size=(400, 400))
-    #     wp.open()
 
 class ResultWindow(Screen):
-    pass
+    def reset(self):
+        ref_to_First_Window = self.manager.get_screen("first")
+        ref_to_First_Window.ids.Him.text = 'Him'
+        ref_to_First_Window.ids.Her.text = 'Her'
+        self.ids.Button_Image.source = 'images/Reset_on_press.png'
+    
+    def release(self):
+        self.ids.Button_Image.source = 'images/Reset_on_release.png'
+
 
 class WindowManager(ScreenManager):
     pass
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-# return self.pos_flame
+mport kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import StringProperty
